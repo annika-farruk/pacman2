7550e30d8eb7dceea9790c3bf769aba0d7e35e92
annika <75987723+annika-farruk@users.noreply.github.com>
2021-08-28 11:41:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,104 @@
+var wall1 , wall2 , wall3, wall4, wall5 , wall6 , wall7 ,wall8 , wall9 , wall10 ;
+var wall11, wall12 ,wall13 ,wall14, wall15 , wall16 , wall17 , wall18 , wall19 , wall20 ;
+var wall21 , wall22 , wall23 , wall24 , wall25 , wall26 , wall27 , wall28 , wall29 , wall30 ;
+var wall31 , wall32 , wall33 , wall34 , wall35 , wall36 , wall37,wall38, wall39, wall40;
+var wall41 ,wall42, wall43;
+
+function setup(){
+  createCanvas(1304,661);
+  wall1 = createSprite(500,600,10,120);
+  wall1.shapeColor = "yellow";
+  wall2 = createSprite(285,300,10,120);
+  wall2.shapeColor = "yellow";
+  wall3 = createSprite(340,245,120,10);
+  wall3.shapeColor = "yellow";
+  wall4 = createSprite(400,300,10,120);
+  wall4.shapeColor = "yellow";
+  wall5 = createSprite(340,300,120,10);
+  wall5.shapeColor = "yellow";
+  wall6 = createSprite(500,300,10,120);
+  wall6.shapeColor = "yellow";
+  wall7 = createSprite(540,260,70,10);
+  wall7.shapeColor = "yellow";
+  wall8 = createSprite(570,310,10,100);
+  wall8.shapeColor = "yellow";
+  wall9 = createSprite(670,310,10,100);
+  wall9.shapeColor = "yellow";
+  wall10 = createSprite(710,355,80,10);
+  wall10.shapeColor = "yellow";
+  wall11 = createSprite(810,360,10,200);
+  wall11.shapeColor = "yellow";
+  wall12 = createSprite(760,455,100,10);
+  wall12.shapeColor = "yellow";
+
+  wall13 = createSprite(652,5,1304,10);
+  wall13.shapeColor = "yellow";
+  wall14 = createSprite(650,660,1304,10);
+  wall14.shapeColor = "yellow";
+  wall15 = createSprite(5,330,10,661);
+  wall15.shapeColor = "yellow";
+  wall16 = createSprite(1300,330,10,661);
+  wall16.shapeColor = "yellow";
+  wall17 = createSprite(100,50,10,200);
+  wall17.shapeColor = "yellow";
+  wall18 = createSprite(652,60,10,200);
+  wall18.shapeColor = "yellow";
+  wall19 = createSprite(600,60,100,10);
+  wall19.shapeColor = "yellow";
+  wall20 = createSprite(550,105,10,100);
+  wall20.shapeColor = "yellow";
+  wall21 = createSprite(330,180,290,10);
+  wall21.shapeColor = "yellow";
+  wall22 = createSprite(150,70,100,10);
+  wall22.shapeColor = "yellow";
+  wall23 = createSprite(300,60,10,100);
+  wall23.shapeColor = "yellow";
+  wall24 = createSprite(50,330,100,10);
+  wall24.shapeColor = "yellow";
+  wall25 = createSprite(300,450,400,10);  
+  wall25.shapeColor = "yellow";
+  wall26 = createSprite(205,320,10,130);
+  wall26.shapeColor = "yellow";
+  wall27 = createSprite(250,580,10,90);
+  wall27.shapeColor = "yellow";
+  wall28 = createSprite(850,80,10,150);
+  wall28.shapeColor = "yellow";
+  wall29 = createSprite(1150,60,10,100);
+  wall29.shapeColor = "yellow";
+  wall30 = createSprite(1050,300,300,10);
+  wall30.shapeColor = "yellow";
+  wall31 = createSprite(950,600,10,300);
+  wall31.shapeColor = "yellow";
+  wall32 = createSprite(1070,450,250,10);
+  wall32.shapeColor = "yellow";
+  wall33 = createSprite(1200,520,10,150);
+  wall33.shapeColor = "yellow";
+  wall34 = createSprite(1105,590,180,10);
+  wall34.shapeColor = "yellow";
+  wall35 = createSprite(1020,550,10,70);
+  wall35.shapeColor = "yellow";
+  wall36 = createSprite(1065,520,100,10);
+  wall36.shapeColor = "yellow";
+  wall37 = createSprite(700,600,10,120);
+  wall37.shapeColor = "yellow";
+  wall38 = createSprite(670,625,50,10);
+  wall38.shapeColor = "yellow";
+  wall39 = createSprite(750,570,100,10);
+  wall39.shapeColor = "yellow";
+  wall40 = createSprite(778,355,55,5);
+  wall40.shapeColor = "white";
+  wall41 = createSprite(740,263,127,5);
+  wall41.shapeColor = "white";
+   
+
+
+
+ 
+
+}
+
+function draw(){
+background("black");
+drawSprites();
+}
+
